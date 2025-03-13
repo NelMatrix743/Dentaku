eb416be902a4addc6a87264c8e829747244c7ac0
NelMatrix743 <nelmatrix155@gmail.com>
2025-03-13 22:46:15 +0000

    feat: Implmement math functions for supported arithmetics

---
+++ lib/utils/math_utils.dart
@@ -0,0 +1,22 @@
+// Dentaku math functions
+
+num addOperation(num input1, num input2) {
+  return input1 + input2;
+}
+
+num subtractOperation(num input1, num input2) {
+  return input1 - input2;
+}
+
+num multiplicationOperation(num input1, input2) {
+  return input1 * input2;
+}
+
+num divisionOperation(num input1, num input2) {
+  String result = (input1 / input2).toString();
+  List<String> tokens = result.split(".");
+  if (int.parse(tokens[1]) > 0) {
+    return (input1 / input2);
+  }
+  return double.parse(result).toInt(); // remove decimal part
+}

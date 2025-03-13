39ad4876f49c4b0a9777d39d613e4b99b47411d4
NelMatrix743 <nelmatrix155@gmail.com>
2025-03-13 22:48:06 +0000

    improve: Implement the optimized version of the divisionOperation() function

--- lib/utils/math_utils.dart
+++ lib/utils/math_utils.dart
@@ -12,11 +12,16 @@ num multiplicationOperation(num input1, input2) {
   return input1 * input2;
 }
 
+// num divisionOperation(num input1, num input2) {
+//   String result = (input1 / input2).toString();
+//   List<String> tokens = result.split(".");
+//   if (int.parse(tokens[1]) > 0) {
+//     return (input1 / input2);
+//   }
+//   return double.parse(result).toInt(); // remove decimal part
+// }
+
 num divisionOperation(num input1, num input2) {
-  String result = (input1 / input2).toString();
-  List<String> tokens = result.split(".");
-  if (int.parse(tokens[1]) > 0) {
-    return (input1 / input2);
-  }
-  return double.parse(result).toInt(); // remove decimal part
+  num result = input1 / input2;
+  return result % 1 == 0 ? result.toInt() : result;
 }
